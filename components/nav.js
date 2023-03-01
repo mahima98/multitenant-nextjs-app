07f0b375eeaae33e57dd3ec452bcbea35c8954d8
mahima98 <mahimaramgolam98@gmail.com>
2023-03-01 16:28:55 +0400

    Added page + global tailwind setup in progress

--- components/nav.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-const Nav = () => <nav></nav>;
-
-export default Nav;
---
+++ pages/homepage/pages/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Nav from "../components/nav";
+import TestComponent from "../../../components/testComponent";
+export default function Homepage() {
+  return (
+    <div className="">
+      <Nav />
+      <TestComponent />
+      Homepage
+    </div>
+  );
+}
